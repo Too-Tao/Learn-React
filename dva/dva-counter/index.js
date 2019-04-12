8dff621b660e772654d95d3ad7917ed025a75853
Too-Tao <599366020@qq.com>
2019-04-12 14:17:34 +0800

    dva-counter

--- dva/dva-counter/index.js
+++ dva/dva-counter/index.js
@@ -1,7 +1,41 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import dva, { connect } from 'dva'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = dva()
 
+console.log(2)
 
+app.model({
+  namespace: 'count',
+  state: 0,
+  reducers: {
+    add (count) { return count + 1 },
+    minus(count) { return count - 1 }
+  }
+})
+
+class TestError extends React.Component {
+  componentDidCatch(e) {
+    alert(e.message)
+  }
+  render() {
+    return <div>TestError</div>
+  }
+}
+
+const App = connect( ({ count }) => ({
+  count
+})) (function(props) {
+  return (
+    <div>
+      <TestError />
+      <h2>{ props.count }</h2>
+      <button key="add" onClick={ () => { props.dispatch({type: 'count/add'})} }>+</button>
+      <button key="minus" onClick={ () => { props.dispatch({type: 'count/minus'})} }>-</button>
+    </div>
+  )
+})
+
+app.router( () => <App />)
+
+app.start('#root')
