f1ffbd1c88204098ba17eeaf070471f971ad76ad
Too-Tao <599366020@qq.com>
2019-04-12 17:06:34 +0800

    add layout header

--- dva/create-umi/src/layouts/index.tsx
+++ dva/create-umi/src/layouts/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header'
 import styles from './index.css';
 
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      <Header location={props.location}/>
       {props.children}
     </div>
   );
